34ee32b3e176cf0a26a9a81d9a29c022f1411b46
Jimmy <jimmylanbcn@gmail.com>
2021-04-26 14:51:37 -0400

    Render transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './transaction.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -19,6 +21,13 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: "t1", title: "New Shoes", amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: "t2", title: "Groceries", amount: 14.99, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,8 +40,8 @@ class MyHomePage extends StatelessWidget {
             child: Text("Chart"),
             elevation: 5,
           ),
-          Card(
-            child: Text("Main List"),
+          Column(
+            children: transactions.map((e) => Text(e.title)).toList(),
           )
         ],
       ),
