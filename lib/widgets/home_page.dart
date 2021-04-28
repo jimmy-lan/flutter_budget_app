ccb430b1e70b0198ed13715786487c2e36c00432
Jimmy <jimmylanbcn@gmail.com>
2021-04-28 13:33:07 -0400

    Refactor view

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -12,10 +12,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [
-    // Transaction(
-    //     id: "t1", title: "New Shoes", amount: 69.99, date: DateTime.now()),
-    // Transaction(
-    //     id: "t2", title: "Groceries", amount: 14.99, date: DateTime.now())
+    Transaction(
+        id: "t1", title: "New Shoes", amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: "t2", title: "Groceries", amount: 14.99, date: DateTime.now())
   ];
 
   void _addNewTransaction(String title, double amount) {
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,8 +10,7 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
+    return Expanded(
       child: transactions.isEmpty
           ? Column(
               children: [
